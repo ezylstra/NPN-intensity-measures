0de8405e7462d2d96cb6c235e43c8806bcec42d7
Erin Zylstra <erinzylstra@gmail.com>
2025-09-26 16:30:43 -0700

    Process downloaded PRISM data and save weather vars

--- redbud-data-exploration.R
+++ redbud-data-exploration.R
@@ -5,8 +5,11 @@ library(tidyverse)
 library(leaflet)
 library(elevatr)
 library(sf)
-# library(ordbetareg)   # Loads brms 
-# library(tidybayes)    # Manipulate Stan objects in a tidy way
+library(terra)
+
+# Specify location of PRISM data ----------------------------------------------#
+
+prism_folder <- "C:/Users/erin/Documents/PRISMData/"
 
 # Load and format status-intensity data ---------------------------------------#
 
@@ -259,71 +262,71 @@ flowersw <- flowersw %>%
 
 # Plot intensity values -------------------------------------------------------#
 
-# How much data each year?
-flowersw %>%
-  group_by(yr) %>%
-  summarize(n_plants = n_distinct(id),
-            n_sites = n_distinct(site_id),
-            n_obs = n(),
-            n_int_flowers = sum(!is.na(intensity_flowers)),
-            n_int_open = sum(!is.na(intensity_open)),
-            n_int_both = sum(!is.na(intensity_flowers) & !is.na(intensity_open))) %>%
-  data.frame()
-# Way more data in 2022-2025 than previous years
-
-# Plot mean daily intensity values for 2022 (was going to use median, but 
-# there are too many 0s)
-flowersw %>%
-  filter(yr == 2022) %>%
-  group_by(doy) %>%
-  summarize(flowers = mean(intensity_flowers, na.rm = TRUE),
-            open = mean(intensity_open, na.rm = TRUE)) %>%
-  mutate(log_flowers = ifelse(flowers == 0, log(flowers + 0.1), log(flowers))) %>%
-  select(-flowers) %>%
-  pivot_longer(cols = c(log_flowers, open),
-               names_to = "type",
-               values_to = "intensity") %>%
-  filter(doy %in% 25:200) %>%
-  ggplot(aes(x = doy, y = intensity)) +
-  geom_line() +
-  facet_grid(type ~ ., scales = "free_y")
-# Messy. However, even here it looks like we can have moderate %open values
-# late in the flowering season when the number of flowers decreases 
-# Try weekly?
-flowersw %>%
-  filter(yr == 2022) %>%
-  mutate(wk = as.numeric(week(observation_date))) %>%
-  group_by(wk) %>%
-  summarize(flowers = mean(intensity_flowers, na.rm = TRUE),
-            open = mean(intensity_open, na.rm = TRUE)) %>%
-  mutate(log_flowers = ifelse(flowers == 0, log(flowers + 0.1), log(flowers))) %>%
-  select(-flowers) %>%
-  pivot_longer(cols = c(log_flowers, open),
-               names_to = "type",
-               values_to = "intensity") %>%
-  filter(wk %in% 9:25) %>%
-  ggplot(aes(x = wk, y = intensity)) +
-  geom_line() +
-  facet_grid(type ~ ., scales = "free_y")
-# Looking at weekly values averaged across individuals, this seems like less of 
-# a problem...
-
-# What about looking at data for an individual?
-count(filter(flowersw, yr == 2022), id) %>% arrange(desc(n)) %>% head(10)
-flowersw %>%
-  filter(yr == 2022 & id == 287224) %>%
-  mutate(flowers_log = ifelse(intensity_flowers == 0, 
-                              log(intensity_flowers + 0.1), 
-                              log(intensity_flowers))) %>%
-  select(-intensity_flowers) %>%
-  pivot_longer(cols = c(flowers_log, intensity_open),
-               names_to = "type",
-               values_to = "intensity") %>%
-  # filter(doy %in% 25:200) %>%
-  ggplot(aes(x = doy, y = intensity)) +
-  geom_line() +
-  facet_grid(type ~ ., scales = "free_y")
-# %open stays high as number of flowers drops off
+# # How much data each year?
+# flowersw %>%
+#   group_by(yr) %>%
+#   summarize(n_plants = n_distinct(id),
+#             n_sites = n_distinct(site_id),
+#             n_obs = n(),
+#             n_int_flowers = sum(!is.na(intensity_flowers)),
+#             n_int_open = sum(!is.na(intensity_open)),
+#             n_int_both = sum(!is.na(intensity_flowers) & !is.na(intensity_open))) %>%
+#   data.frame()
+# # Way more data in 2022-2025 than previous years
+# 
+# # Plot mean daily intensity values for 2022 (was going to use median, but 
+# # there are too many 0s)
+# flowersw %>%
+#   filter(yr == 2022) %>%
+#   group_by(doy) %>%
+#   summarize(flowers = mean(intensity_flowers, na.rm = TRUE),
+#             open = mean(intensity_open, na.rm = TRUE)) %>%
+#   mutate(log_flowers = ifelse(flowers == 0, log(flowers + 0.1), log(flowers))) %>%
+#   select(-flowers) %>%
+#   pivot_longer(cols = c(log_flowers, open),
+#                names_to = "type",
+#                values_to = "intensity") %>%
+#   filter(doy %in% 25:200) %>%
+#   ggplot(aes(x = doy, y = intensity)) +
+#   geom_line() +
+#   facet_grid(type ~ ., scales = "free_y")
+# # Messy. However, even here it looks like we can have moderate %open values
+# # late in the flowering season when the number of flowers decreases 
+# # Try weekly?
+# flowersw %>%
+#   filter(yr == 2022) %>%
+#   mutate(wk = as.numeric(week(observation_date))) %>%
+#   group_by(wk) %>%
+#   summarize(flowers = mean(intensity_flowers, na.rm = TRUE),
+#             open = mean(intensity_open, na.rm = TRUE)) %>%
+#   mutate(log_flowers = ifelse(flowers == 0, log(flowers + 0.1), log(flowers))) %>%
+#   select(-flowers) %>%
+#   pivot_longer(cols = c(log_flowers, open),
+#                names_to = "type",
+#                values_to = "intensity") %>%
+#   filter(wk %in% 9:25) %>%
+#   ggplot(aes(x = wk, y = intensity)) +
+#   geom_line() +
+#   facet_grid(type ~ ., scales = "free_y")
+# # Looking at weekly values averaged across individuals, this seems like less of 
+# # a problem...
+# 
+# # What about looking at data for an individual?
+# count(filter(flowersw, yr == 2022), id) %>% arrange(desc(n)) %>% head(10)
+# flowersw %>%
+#   filter(yr == 2022 & id == 287224) %>%
+#   mutate(flowers_log = ifelse(intensity_flowers == 0, 
+#                               log(intensity_flowers + 0.1), 
+#                               log(intensity_flowers))) %>%
+#   select(-intensity_flowers) %>%
+#   pivot_longer(cols = c(flowers_log, intensity_open),
+#                names_to = "type",
+#                values_to = "intensity") %>%
+#   # filter(doy %in% 25:200) %>%
+#   ggplot(aes(x = doy, y = intensity)) +
+#   geom_line() +
+#   facet_grid(type ~ ., scales = "free_y")
+# # %open stays high as number of flowers drops off
 
 # Calculate estimated number of open flowers ----------------------------------#
 
@@ -351,53 +354,207 @@ of <- flowersw %>%
   filter(start0 == 1 & end0 == 1) %>%
   data.frame()
 
+# Merge with site information, and limit data to 2016-2025
+of <- of %>%
+  # Get rid of some columns we no longer need
+  select(-c(nonzero_counts, min_doy, start0, end0)) %>%
+  left_join(select(sites, site_id, state, lat, lon, elev), 
+            by = "site_id") %>%
+  filter(yr > 2015)
+
+# Extract coordinates for remaining sites
+peaksites <- distinct(of, site_id, lat, lon)
+
 # Basic summaries/visualizations of open flower abundance data ----------------#
 
-# What do the values look like?
-count(of, nopen)
-# How much data do we have each year?
-of %>%
-  group_by(yr) %>%
-  summarize(nplants = n_distinct(id),
-            nobs = n()) %>%
-  mutate(nobsperplant = nobs / nplants) %>%
-  data.frame()
-# 2016-2025 look pretty good.
+# # What do the values look like?
+# count(of, nopen)
+# # How much data do we have each year?
+# of %>%
+#   group_by(yr) %>%
+#   summarize(nplants = n_distinct(id),
+#             nobs = n()) %>%
+#   mutate(nobsperplant = nobs / nplants) %>%
+#   data.frame()
+# 
+# # Look at data from 2016 (first year with >= 10 plants)
+# ggplot(filter(of, yr == 2016), aes(x = doy, y = nopen)) +
+#   geom_line() +
+#   geom_point(color = "blue") +
+#   facet_wrap(~id)
+# # There are massive differences in max counts among plants within and across
+# # years.
+# ggplot(filter(of, yr == 2016), aes(x = doy, y = log(nopen + 0.1))) +
+#   geom_line() +
+#   geom_point(color = "blue") +
+#   facet_wrap(~id)
+# 
+# # Look at variation in max counts among years for a given plant
+# of %>%
+#   group_by(id) %>%
+#   summarize(nyrs = n_distinct(yr)) %>%
+#   arrange(desc(nyrs)) %>%
+#   head(20)
+# 
+# plant <- 42681 # 25365
+# of %>%
+#   filter(id == plant) %>%
+#   group_by(yr) %>%
+#   summarize(yrmax = max(nopen)) %>%
+#   data.frame()
+# of %>%
+#   filter(id == plant) %>%
+#   ggplot(aes(x = doy, y = nopen)) +
+#   geom_line() +
+#   geom_point(color = "steelblue3") +
+#   facet_wrap(~yr)
+# # Occasionally, max values increase over time, potentially indicating the plant
+# # was small/young in first years (eg, 46192). But most often, max values vary
+# # with no trend.
 
-# Look at data from 2016 (first year with >= 10 plants)
-ggplot(filter(of, yr == 2016), aes(x = doy, y = nopen)) +
-  geom_line() +
-  geom_point(color = "blue") +
-  facet_wrap(~id)
-ggplot(filter(of, yr == 2016), aes(x = doy, y = log(nopen + 0.1))) +
-  geom_line() +
-  geom_point(color = "blue") +
-  facet_wrap(~id)
-# There are massive differences in max counts among plants within and across 
-# years. 
+# Create weather covariates for each site -------------------------------------#
+# All data from PRISM (temps in degC and precip in mm)
+# This section only needs to be run once:
 
-# Look at variation in max counts among years for a given plant
-of %>%
-  group_by(id) %>%
-  summarize(nyrs = n_distinct(yr)) %>%
-  arrange(desc(nyrs)) %>%
-  head(20)
+update_weather <- FALSE
+weather_missing <- ifelse(length(list.files("weather-data/redbud")) == 0, 
+                          TRUE, FALSE)
 
-plant <- 232170 # 25365
-of %>%
-  filter(id == plant) %>%
-  group_by(yr) %>%
-  summarize(yrmax = max(nopen)) %>%
-  data.frame()
-of %>%
-  filter(id == plant) %>%
-  ggplot(aes(x = doy, y = nopen)) +
-  geom_line() +
-  geom_point(color = "steelblue3") +
-  facet_wrap(~yr)
-# Occasionally, max values increase over time, potentially indicating the plant
-# was small/young in first years (eg, 46192). But most often, max values vary 
-# with no trend.
+if(update_weather | weather_missing) {
+  
+  # First, put site locations to a SpatVector, and convert to NAD83 to match
+  # PRISM data
+  peaksitesv <- vect(peaksites, geom = c("lon", "lat"), crs = "epsg:4326")
+  # Convert to NAD83 to match PRISM data
+  peaksitesv <- terra::project(peaksitesv, "epsg:4269")
+  
+  # Specify years
+  yrs <- 2016:2025
+  
+  for (yr in yrs) {
+  
+    # Precipitation for winter (Dec-Feb) and 6-mo (Nov-Apr) ---------------------#
+    # Get daily precipitation in Nov-Dec of year prior
+    rppt <- readRDS(paste0(prism_folder, "ppt-", yr - 1, ".rds"))
+    rppt_nov <- rppt[[str_sub(names(rppt), 9, 10) == "11"]]
+    ppt_nov <- terra::extract(rppt_nov, peaksitesv, ID = FALSE)
+    rppt_dec <- rppt[[str_sub(names(rppt), 9, 10) == "12"]]
+    ppt_dec <- terra::extract(rppt_dec, peaksitesv, ID = FALSE)
+    
+    # Get daily precipitation in Jan-Apr of same year
+    rppt <- readRDS(paste0(prism_folder, "ppt-", yr, ".rds"))
+    rppt_jan <- rppt[[str_sub(names(rppt), 9, 10) == "01"]]
+    ppt_jan <- terra::extract(rppt_jan, peaksitesv, ID = FALSE)
+    rppt_feb <- rppt[[str_sub(names(rppt), 9, 10) == "02"]]
+    ppt_feb <- terra::extract(rppt_feb, peaksitesv, ID = FALSE)
+    rppt_mar <- rppt[[str_sub(names(rppt), 9, 10) == "03"]]
+    ppt_mar <- terra::extract(rppt_mar, peaksitesv, ID = FALSE)
+    rppt_apr <- rppt[[str_sub(names(rppt), 9, 10) == "04"]]
+    ppt_apr <- terra::extract(rppt_apr, peaksitesv, ID = FALSE)
+    
+    # Get monthly totals
+    ppt <- data.frame(site_id = peaksites$site_id,
+                      season = yr,
+                      nov = apply(ppt_nov, 1, sum),
+                      dec = apply(ppt_dec, 1, sum),
+                      jan = apply(ppt_jan, 1, sum),
+                      feb = apply(ppt_feb, 1, sum),
+                      mar = apply(ppt_mar, 1, sum),
+                      apr = apply(ppt_apr, 1, sum))
+    rm(rppt, rppt_jan, rppt_feb, rppt_mar, rppt_apr, rppt_nov, rppt_dec,
+       ppt_jan, ppt_feb, ppt_mar, ppt_apr, ppt_nov, ppt_dec)
+    
+    # Winter minimum temperatures (Dec-Feb) -------------------------------------#
+    # Get daily min temperatures in Dec of year prior
+    rtmin <- readRDS(paste0(prism_folder, "tmin-", yr - 1, ".rds"))
+    rtmin_dec <- rtmin[[str_sub(names(rtmin), 10, 11) == "12"]]
+    tmin_dec <- terra::extract(rtmin_dec, peaksitesv, ID = FALSE)
+  
+    # Get daily min temperatures in Jan-Feb of same year
+    rtmin <- readRDS(paste0(prism_folder, "tmin-", yr, ".rds"))
+    rtmin_janfeb <- rtmin[[str_sub(names(rtmin), 10, 11) %in% c("01", "02")]]
+    tmin_janfeb <- terra::extract(rtmin_janfeb, peaksitesv, ID = FALSE)
+    
+    tmin_win <- cbind(site_id = peaksites$site_id, tmin_dec, tmin_janfeb)
+    tmin_win <- data.frame(site_id = tmin_win$site_id,
+                           season = yr,
+                           tmin_win = apply(tmin_win[, -1], 1, mean))
+    rm(rtmin_dec, rtmin_janfeb, tmin_dec, tmin_janfeb)
+  
+    # GDD values (base 0) from Jan 1 or Mar 1 -----------------------------------#
+    # Aleady have raster with tmin values; load raster with tmax values
+    rtmax <- readRDS(paste0(prism_folder, "tmax-", yr, ".rds"))
+    # Stack them, and use the tapp() function to create a SpatRaster with the 
+    # mean temp for each day
+    rtmean <- c(rtmin, rtmax)
+    ndays <- nlyr(rtmax)
+    rtmean <- tapp(rtmean, index = c(1:ndays, 1:ndays), fun = "mean")
+    names(rtmean) <- str_replace_all(names(rtmax), "tmax", "tmean")
+    
+    # Extract mean temperatures through end of June
+    rtmean <- rtmean[[str_sub(names(rtmean), 11, 12) %in%
+                        c("01", "02", "03", "04", "05", "06")]]
+    
+    # Calculate daily GDD values, starting on Jan 1
+    gdd_Jan1 <- terra::extract(rtmean, peaksitesv, ID = FALSE)
+    gdd_Jan1[gdd_Jan1 < 0] <- 0
+    # Only need through DOY 180
+    gdd_Jan1_180 <- gdd_Jan1[,1:180]
+    # Will use cumsum(), but note that it works on columns of a dataframe but not 
+    # rows so need to do some transposing
+    gdd_Jan1_t <- t(gdd_Jan1_180) %>% data.frame()
+    agdd_Jan1 <- cumsum(gdd_Jan1_t) %>% t() %>% data.frame()
+    names(agdd_Jan1) <- paste0("d", 1:ncol(agdd_Jan1))
+    agdd_Jan1 <- cbind(site_id = peaksites$site_id,
+                       season = yr,
+                       agdd_Jan1)
+  
+    # Calculate daily GDD values, starting on Mar 1
+    gdd_Mar1 <- gdd_Jan1[!str_sub(names(gdd_Jan1), 11, 12) %in% c("01", "02")]
+    # To make sure all years have the same number of columns
+    gdd_Mar1_t <- t(gdd_Mar1) %>% data.frame()
+    agdd_Mar1 <- cumsum(gdd_Mar1_t) %>% t() %>% data.frame()
+    names(agdd_Mar1) <- str_replace_all(names(agdd_Mar1), paste0("tmean_", yr), "agdd_")
+    agdd_Mar1 <- cbind(site_id = peaksites$site_id,
+                       season = yr,
+                       agdd_Mar1)
+  
+    rm(rtmin, rtmax, rtmean, gdd_Jan1, gdd_Jan1_t, gdd_Jan1_180, 
+       gdd_Mar1, gdd_Mar1_t)
+    
+    if (yr == min(yrs)) {
+      agdd_jan1_all <- agdd_Jan1
+      agdd_mar1_all <- agdd_Mar1
+      tmin_win_all <- tmin_win
+      ppt_all <- ppt
+    } else {
+      agdd_jan1_all <- rbind(agdd_jan1_all, agdd_Jan1)
+      agdd_mar1_all <- rbind(agdd_mar1_all, agdd_Mar1)
+      tmin_win_all <- rbind(tmin_win_all, tmin_win)
+      ppt_all <- rbind(ppt_all, ppt)
+    }
+  }
+  
+  # Save to file
+  write.csv(agdd_jan1_all,
+            "weather-data/redbud/agdd-jan1.csv",
+            row.names = FALSE)
+  write.csv(agdd_mar1_all,
+            "weather-data/redbud/agdd-mar1.csv",
+            row.names = FALSE)
+  write.csv(tmin_win_all,
+            "weather-data/redbud/tmin-win.csv",
+            row.names = FALSE)
+  write.csv(ppt_all,
+            "weather-data/redbud/ppt-monthly.csv",
+            row.names = FALSE)
+}
+
+# Load weather data:
+ppt <- read.csv("weather-data/redbud/ppt-monthly.csv")
+tmin_win <- read.csv("weather-data/redbud/tmin-win.csv")
+agdd_jan1 <- read.csv("weather-data/redbud/agdd-jan1.csv")
+agdd_mar1 <- read.csv("weather-data/redbud/agdd-mar1.csv")
 
 # Modeling variation in max annual counts -------------------------------------# 
 
@@ -441,11 +598,12 @@ ofmax %>%
 
 # Start simple, using lmer with log(maxcount)?
 library(lme4)
-m1 <- lmer(log(maxcount) ~ lat + lon + elev + (1|fyr) + (1|id), data = ofmax)
+m1 <- lmer(log(maxcount) ~ lat + lon + elev + (1|fyr) + (1|site_id), 
+           data = ofmax)
 summary(m1)
 # Max counts increased with latitude; possibly a negative effect of longitude
 # No effect of elevation. 
-# Residual variance greater than random effect of individual
+# Residual variance (individual) greater than site variance
 # Random year effect tiny.
 
 # Would like to get some kind of weather variables (relating to 
@@ -462,14 +620,14 @@ summary(m1)
 # Rauschkolb preprint.
 
 # Do we need to filter out plant-years where there are long gaps between
-# observations -- this could really bias estimates of peak date (eg, 150802 in
+# observations? This could really bias estimates of peak date (eg, 150802 in
 # 2018). Really, we just need to find those series where the gap is adjacent to
 # max values.
 
 of <- of %>%
   arrange(id, observation_date) %>%
-  mutate(interval_before = NA) %>%
   mutate(obsdate = ymd(observation_date)) %>%
+  mutate(interval_before = NA) %>%
   select(-observation_date)
 for (i in 2:nrow(of)) {
   of$interval_before[i] <- ifelse(
@@ -519,4 +677,38 @@ of_plantyr %>%
   summarize(nplants = n(),
             filter30 = n() - sum(filter30),
             filter21 = n() - sum(filter21))
+
+# For now, will use 30-day interval filter
+# Calculate peak date as mean date betewen first and last dates with max count
+of_plantyr30 <- of_plantyr %>%
+  filter(filter30 == 0) %>%
+  select(-c(interval_mn, interval_mx, filter30, filter21)) %>%
+  mutate(max_span = last_max - first_max,
+         peak = floor((first_max + last_max)/2),
+         fyr = factor(yr),
+         yr0 = yr - min(yr),
+         lat_z = (lat - mean(lat)) / sd(lat),
+         lon_z = (lon - mean(lon)) / sd(lon),
+         elev_z = (elev - mean(elev)) / sd(elev))
+ 
+# Really basic model:
+# (note: can't include site id, id, and year REs in the same model. Singular
+# fit warnings because there aren't any DF left for residual error)
+mpeak1 <- lmer(peak ~ lat_z + lon_z + elev_z + yr0 + (1|fyr) + (1|site_id), 
+           data = of_plantyr30)
+summary(mpeak1)
+# As expected, strong positive association of peak date with latitude
+# Marginal, positive elevation effect
+# No effect of longitude
+# No evidence of a trend from 2016-2025
+# Pretty big random year, site, and residual/individual effects 
+
+# Looked at Tariq's paper on timing of flowering onset in eastern redbud.
+# Chilling requirement, starting around mid-Oct
+# Forcing requirement with ~0 deg base, starting after exceeding "photoperiod" 
+# threshold of ~ 11 hours (which is roughly around 1 March for this region)
+
+# Need to get weather data for sites
+peaksites <- distinct(of_plantyr30, site_id, lat, lon)
+
 
